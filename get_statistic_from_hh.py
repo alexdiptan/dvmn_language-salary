c3f92491c68f294e94aab1e266367fd07c9519d1
aleksandrdiptan <diptan@huntflow.ru>
2022-11-06 11:55:50 +0300

    add: - replace print to logging

--- get_statistic_from_hh.py
+++ get_statistic_from_hh.py
@@ -1,3 +1,5 @@
+import logging
+
 import requests as requests
 
 import common_func
@@ -55,7 +57,7 @@ def get_vacancies_from_all_pages(url, pl):
     if count_of_vacancies_pages > 0:
         pl_vacancies_data_template['items'] = []
         for page in range(count_of_vacancies_pages):
-            print(f'Programming language is: {pl}, {page=} from {count_of_vacancies_pages}')
+            logging.info(f'Programming language is: {pl}, {page} page of {count_of_vacancies_pages}')
             vacancies_from_page = get_hh_vacancies(url, pl, page)['items']
             pl_vacancies_data_template['items'].extend(vacancies_from_page)
 
@@ -63,6 +65,7 @@ def get_vacancies_from_all_pages(url, pl):
 
 
 def main():
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
     programming_languages = ['Python', 'Java', 'Javascript']
     url = 'https://api.hh.ru/vacancies'
     languages_info = {}
@@ -70,11 +73,9 @@ def main():
     for pl in programming_languages:
         pl_vacancies = get_vacancies_from_all_pages(url, pl)
         languages_info[pl] = get_statistic_by_pl(pl_vacancies)
-        count_vacancies_by_pl = len(pl_vacancies['items'])
-        print(f'{count_vacancies_by_pl=}')
         common_func.save_to_json(pl_vacancies, f'{pl}.json')
 
-    print(languages_info)
+    logging.info(languages_info)
 
 
 if __name__ == '__main__':
